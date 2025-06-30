c883eb9e544d49d7568a7bb00288940b9a42f85f
eshabansiya <esha_bansiya@berkeley.edu>
2025-06-30 13:35:45 -0700

    new py

---
+++ app.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn as sk
+import scipy as sci
+import plotly.express as px
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import NearestNeighbors
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics.pairwise import cosine_similarity
+
